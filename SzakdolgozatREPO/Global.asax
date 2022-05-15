e626cecd65ddd1d81514363697745ce52c09bb67
adam026 <76121613+adam026@users.noreply.github.com>
2022-05-15 20:08:32 +0200

    Add project files.

---
+++ SzakdolgozatREPO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SzakdolgozatREPO.MvcApplication" Language="C#" %>
---
+++ SzakdolgozatREPO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SzakdolgozatREPO.Startup))]
+namespace SzakdolgozatREPO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
